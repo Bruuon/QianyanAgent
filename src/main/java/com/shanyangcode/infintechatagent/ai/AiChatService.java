81580a3c58a6f7c6a3c1b75fbd35af95fad4e579
Bruuon <MLandXXDD@163.com>
2026-04-04 15:48:40 +0800

    feat: initialization

---
+++ src/main/java/com/shanyangcode/infintechatagent/ai/AiChatService.java
@@ -0,0 +1,40 @@
+package com.shanyangcode.infintechatagent.ai;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class AiChatService {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Resource
+    private EmbeddingStore<TextSegment> embeddingStore;
+
+    @Bean
+    public AiChat aiChat() {
+
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments("src/main/resources/docs");
+        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+
+        return AiServices.builder(AiChat.class)
+                .chatModel(chatModel)
+                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
+                .chatMemoryProvider(memoryId -> MessageWindowChatMemory.withMaxMessages(10))
+                .build();
+    }
+
+}
