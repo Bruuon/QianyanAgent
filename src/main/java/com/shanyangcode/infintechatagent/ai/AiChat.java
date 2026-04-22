c7773cd80d31080974568add8ee9b63d4e9fb595
Bruuon <MLandXXDD@163.com>
2026-04-22 22:33:52 +0800

    chore: update log level into OFF

--- src/main/java/com/shanyangcode/infintechatagent/ai/AiChat.java
+++ src/main/java/com/shanyangcode/infintechatagent/ai/AiChat.java
@@ -5,23 +5,16 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.guardrail.InputGuardrails;
-<<<<<<< HEAD
-=======
 import reactor.core.publisher.Flux;
->>>>>>> 7541c4b (add config)
 
 
 @InputGuardrails({SafeInputGuardrail.class})
 public interface AiChat {
 
     @SystemMessage(fromResource = "system-prompt/chat-bot.txt")
-<<<<<<< HEAD
-    String chat(@MemoryId String sessionId, @UserMessage String prompt);
-=======
     String chat(@MemoryId Long sessionId, @UserMessage String prompt);
 
 
     @SystemMessage(fromResource = "system-prompt/chat-bot.txt")
     Flux<String> streamChat(@MemoryId Long sessionId, @UserMessage String prompt);
->>>>>>> 7541c4b (add config)
 }
