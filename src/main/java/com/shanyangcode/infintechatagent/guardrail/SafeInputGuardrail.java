ac23fbb1bf0f80c557f6818293d3cdf94e166b86
harudorobou1145 <834850709@qq.com>
2026-04-26 20:09:16 +0800

    feat: improve sensitive word system
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/shanyangcode/infinitechatagent/ai/AiChatService.java
    #	src/main/java/com/shanyangcode/infintechatagent/guardrail/SafeInputGuardrail.java
    #	src/main/resources/application.yml

--- src/main/java/com/shanyangcode/infintechatagent/guardrail/SafeInputGuardrail.java
+++ src/main/java/com/shanyangcode/infintechatagent/guardrail/SafeInputGuardrail.java
@@ -1,5 +1,6 @@
 package com.shanyangcode.infintechatagent.guardrail;
 
+import com.github.houbb.sensitive.word.core.SensitiveWordHelper;
 import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.guardrail.InputGuardrail;
 import dev.langchain4j.guardrail.InputGuardrailResult;
@@ -9,18 +10,18 @@ import java.util.Set;
 public class SafeInputGuardrail implements InputGuardrail {
 
 
-    private static final Set<String> sensitiveWords = Set.of("死", "杀");
+
 
     @Override
     public InputGuardrailResult validate(UserMessage userMessage) {
         String inputText = userMessage.singleText();
 
 
-        for (String keyword : sensitiveWords) {
-            if (!keyword.isEmpty() && inputText.contains(keyword)) {
+
+            if (SensitiveWordHelper.contains(inputText)) {
                 return fatal("提问不能包含敏感词！！！！！");
             }
-        }
+        
 
         return success();
     }
